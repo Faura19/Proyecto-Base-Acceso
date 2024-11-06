bd30dde3d2a7141b7250247a83f98b7400a66b7b
Faura19 <161851845+Faura19@users.noreply.github.com>
2024-11-06 20:54:19 +0100

    Add files via upload

---
+++ src/main/java/com/spaceinvaders/spaceinvaders/Rocket.java
@@ -0,0 +1,123 @@
+package com.spaceinvaders.spaceinvaders;
+
+import javafx.application.Platform;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import java.io.Serializable;
+
+import static com.spaceinvaders.spaceinvaders.SpaceInvaders.*;
+
+// Player
+public class Rocket implements Serializable {
+    int score;
+    int posX, posY, size;
+    boolean exploding, destroyed;
+    int imgIndex;
+    int explosionStep = 0;
+    private HiloMusical hiloMusical;
+
+    public Rocket(int posX, int posY, int size) {
+        this.posX = posX;
+        this.posY = posY;
+        this.size = size;
+    }
+
+    public Rocket(){
+
+    }
+
+    public void explosionDisparos() {
+        try {
+            File explosionSoundFile = new File("src/door-bang-1wav-14449.wav");  // Ajusta la ruta según la ubicación de tu archivo
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(explosionSoundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();  // Reproducir el sonido de explosión
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void explosionCohete() {
+        try {
+            File explosionSoundFile = new File("src/081895_impact_wav-43951.wav");  // Ajusta la ruta según la ubicación de tu archivo
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(explosionSoundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();  // Reproducir el sonido de explosión
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void muerteNave() {
+        try {
+            File explosionSoundFile = new File("src/mixkit-arcade-retro-game-over-213.wav");  // Ajusta la ruta según la ubicación de tu archivo
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(explosionSoundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();  // Reproducir el sonido de explosión
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Shot shoot() {
+        explosionDisparos();
+        return new Shot(posX + size / 2 - Shot.size / 2, posY - Shot.size);
+    }
+
+    public void update() {
+        if(exploding) explosionStep++;
+        destroyed = explosionStep > EXPLOSION_STEPS;
+    }
+
+
+
+    public void draw() {
+        if(exploding) {
+            gc.drawImage(EXPLOSION_IMG, explosionStep % EXPLOSION_COL * EXPLOSION_W, (explosionStep / EXPLOSION_ROWS) * EXPLOSION_H + 1,
+                    EXPLOSION_W, EXPLOSION_H,
+                    posX, posY, size, size);
+
+            muerteNave();
+
+        }
+        else {
+            gc.drawImage(PLAYER_IMG, posX, posY, size, size);
+        }
+    }
+
+    public boolean colide(Rocket other) {
+        int d = distance(this.posX + size / 2, this.posY + size /2,
+                other.posX + other.size / 2, other.posY + other.size / 2);
+        return d < other.size / 2 + this.size / 2 ;
+    }
+
+    public void explode() {
+        exploding = true;
+        explosionStep = -1;
+
+        explosionCohete();
+
+        /*
+        String cancion1="src/clip-1-muzica-pentru-reclame-" +
+                "film-calitate-24-biti-fisier" +
+                "-audio-wav-viteza-124-bpm-durata-105-min-90.wav";
+
+        String cancion2="src/clip-2-muzica-pentru-reclama-film-24-biti-wav-120-bpm-9013.wav";
+
+        HiloMusical hiloMusical=new HiloMusical(cancion1,cancion2);
+
+        if (hiloMusical!=null){
+            hiloMusical.detenerMusica();
+        }
+         */
+    }
+}
---
+++ src/main/java/com/spaceinvaders/spaceinvaders/SpaceInvaders.java
@@ -0,0 +1,436 @@
+package com.spaceinvaders.spaceinvaders;
+
+import java.io.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import javax.sound.sampled.Clip;
+
+public class SpaceInvaders extends Application implements Serializable{
+
+	private Clip clip;
+	private HiloMusical hiloMusical;
+
+	private static SpaceInvaders instancia;
+	private boolean juegoPausado;
+	private List<String> partidasListView;
+	private int contador;
+
+	public SpaceInvaders(){
+		instancia=this;
+	}
+
+	public static SpaceInvaders getInstancia(){
+		return instancia;
+	}
+
+	public void juegoParado(){
+		juegoPausado=true;
+
+	}
+
+	public void juegoReanudado(){
+		juegoPausado=false;
+	}
+
+	private boolean menuVisible=false;
+
+	// Variables
+	static final Random RAND = new Random();
+	static final int WIDTH = 800;
+	static final int HEIGHT = 600;
+	static final int PLAYER_SIZE = 60;
+	static final Image PLAYER_IMG = new Image("file:images/player.png");
+	static final Image EXPLOSION_IMG = new Image("file:images/explosion.png");
+	static final int EXPLOSION_W = 128;
+	static final int EXPLOSION_ROWS = 3;
+	static final int EXPLOSION_COL = 3;
+	static final int EXPLOSION_H = 128;
+	static final int EXPLOSION_STEPS = 15;
+	static GraphicsContext gc;
+
+	static final Image BOMBS_IMG[] = {
+			new Image("file:images/1.png"),
+			new Image("file:images/2.png"),
+			new Image("file:images/3.png"),
+			new Image("file:images/4.png"),
+			new Image("file:images/5.png"),
+			new Image("file:images/6.png"),
+			new Image("file:images/7.png"),
+			new Image("file:images/8.png"),
+			new Image("file:images/9.png"),
+			new Image("file:images/10.png"),
+	};
+	final int MAX_BOMBS = 10,  MAX_SHOTS = MAX_BOMBS * 2;
+	boolean gameOver = false;
+
+	static Rocket player;
+	List<Shot> shots;
+	List<Universe> univ;
+	List<Bomb> Bombs;
+
+	private double mouseX;
+	private Stage mainStage;
+	private StackPane rootPane;
+
+	// Start
+	public void start(Stage stage) throws Exception {
+		this.mainStage=stage;
+
+		String cancion1="src/clip-1-muzica-pentru-reclame-" +
+				"film-calitate-24-biti-fisier" +
+				"-audio-wav-viteza-124-bpm-durata-105-min-90.wav";
+
+		String cancion2="src/clip-2-muzica-pentru-reclama-film-24-biti-wav-120-bpm-9013.wav";
+
+		hiloMusical=new HiloMusical(cancion1,cancion2);
+		hiloMusical.reproducirMusica(cancion1,cancion2);
+
+
+		// Este metodo sirve para que al cerrar el javafx la musica pare
+		stage.setOnCloseRequest(event -> {
+			hiloMusical.detenerMusica();
+			Platform.exit();
+			System.exit(0);
+		});
+
+		Canvas canvas = new Canvas(WIDTH, HEIGHT);
+		gc = canvas.getGraphicsContext2D();
+		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.play();
+		canvas.setCursor(Cursor.MOVE);
+
+		canvas.setOnMouseMoved(e -> mouseX = e.getX());
+		canvas.setOnMouseClicked(e -> {
+			if(shots.size() < MAX_SHOTS) shots.add(player.shoot());
+			if(gameOver) {
+				gameOver = false;
+				setup();
+			}
+		});
+		setup();
+
+
+		Scene scene=new Scene(new StackPane(canvas));
+		scene.setOnKeyPressed(event->{
+			if(event.getCode()==KeyCode.X){
+				hiloMusical.detenerMusica();
+				Platform.exit();
+			} else if(event.getCode()==KeyCode.P){
+				hiloMusical.detenerMusica();
+				timeline.pause();
+
+			} else if (event.getCode()==KeyCode.O) {
+				timeline.play();
+				hiloMusical.reproducirMusica(cancion1,cancion2);
+
+			} else if(event.getCode()==KeyCode.ESCAPE){
+
+				try {
+					FXMLLoader loader=new FXMLLoader(getClass().getResource("menu.fxml"));
+					Stage menuRoot=new Stage();
+
+					Scene rootPane=new Scene(loader.load());
+
+					juegoParado();
+					//hiloMusical.detenerMusica();
+					menuRoot.setScene(rootPane);
+					menuRoot.show();
+
+					Menu menu=new Menu();
+
+
+					//menu.continuarAction(timeline.play());
+
+					if (menu.pararMusica==true){
+						hiloMusical.detenerMusica();
+					}
+
+					Bombs.stream().peek(Rocket::update).peek(Rocket::draw).forEach(e -> {
+						if(player.colide(e) && !player.exploding) {
+							hiloMusical.detenerMusica();
+						}
+					});
+				}catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
+
+		stage.setScene(scene);
+		stage.setTitle("Space Invaders");
+		stage.show();
+	}
+
+	public void quitarMusica(){
+		hiloMusical.detenerMusica();
+	}
+
+	public void guardarEstadoJuego() {
+		String[] rutas= {"src/savegame.dat","src/savedgame2.dat","src/savedgame3.dat",
+				"src/savedgame4.dat","savedgame5.dat"};
+
+
+		if (contador<rutas.length){
+			try (FileOutputStream ruta = new FileOutputStream(rutas[contador]);
+				 ObjectOutputStream object = new ObjectOutputStream(ruta)) {
+
+				object.writeObject(player);
+				object.writeObject(shots);
+				object.writeObject(Bombs);
+				object.writeObject(univ);
+
+				contador++;
+				System.out.println("Partida" + contador);
+
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else{
+			System.out.println("No se pueden guardar mas partidas");
+		}
+		juegoParado();
+	}
+
+	public void cargarEstadoJuego() {
+		try (FileInputStream ruta = new FileInputStream("src/savegame.dat");
+			 ObjectInputStream fichero = new ObjectInputStream(ruta)) {
+
+			player = (Rocket) fichero.readObject();
+			shots = (List<Shot>) fichero.readObject();
+			Bombs = (List<Bomb>) fichero.readObject();
+			univ= (List<Universe>) fichero.readObject();
+
+			System.out.println("El estado del juego ha sido cargado desde " + "src/savegame.dat");
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		juegoReanudado();
+	}
+
+	public void cargarEstadoJuego2() {
+		try (FileInputStream ruta = new FileInputStream("src/savedgame2.dat");
+			 ObjectInputStream fichero = new ObjectInputStream(ruta)) {
+
+			player = (Rocket) fichero.readObject();
+			shots = (List<Shot>) fichero.readObject();
+			Bombs = (List<Bomb>) fichero.readObject();
+			univ= (List<Universe>) fichero.readObject();
+
+			System.out.println("El estado del juego ha sido cargado desde " + "src/savegame.dat");
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		juegoReanudado();
+	}
+
+	public void cargarEstadoJuego3() {
+		try (FileInputStream ruta = new FileInputStream("src/savedgame3.dat");
+			 ObjectInputStream fichero = new ObjectInputStream(ruta)) {
+
+			player = (Rocket) fichero.readObject();
+			shots = (List<Shot>) fichero.readObject();
+			Bombs = (List<Bomb>) fichero.readObject();
+			univ= (List<Universe>) fichero.readObject();
+
+			System.out.println("El estado del juego ha sido cargado desde " + "src/savegame.dat");
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		juegoReanudado();
+	}
+
+	public void cargarEstadoJuego4() {
+		try (FileInputStream ruta = new FileInputStream("src/savedgame4.dat");
+			 ObjectInputStream fichero = new ObjectInputStream(ruta)) {
+
+			player = (Rocket) fichero.readObject();
+			shots = (List<Shot>) fichero.readObject();
+			Bombs = (List<Bomb>) fichero.readObject();
+			univ= (List<Universe>) fichero.readObject();
+
+			System.out.println("El estado del juego ha sido cargado desde " + "src/savegame.dat");
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		juegoReanudado();
+	}
+
+	public void cargarEstadoJuego5() {
+		try (FileInputStream ruta = new FileInputStream("src/savedgame5.dat");
+			 ObjectInputStream fichero = new ObjectInputStream(ruta)) {
+
+			player = (Rocket) fichero.readObject();
+			shots = (List<Shot>) fichero.readObject();
+			Bombs = (List<Bomb>) fichero.readObject();
+			univ= (List<Universe>) fichero.readObject();
+
+			System.out.println("El estado del juego ha sido cargado desde " + "src/savegame.dat");
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		juegoReanudado();
+	}
+
+
+	public void resumeGame() {
+		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+
+		if (timeline != null) {
+			timeline.play();
+		}
+
+	}
+
+	// Setup the game, only executed once
+	private void setup() {
+		univ = new ArrayList<>();
+		shots = new ArrayList<>();
+		Bombs = new ArrayList<>();
+		player = new Rocket(WIDTH / 2, HEIGHT - PLAYER_SIZE, PLAYER_SIZE);
+		player.score = 0;
+		IntStream.range(0, MAX_BOMBS).mapToObj(i -> this.newBomb()).forEach(Bombs::add);
+	}
+
+	private void playMusic(){
+
+		/*
+		try {
+			File archivoSonido=new File("src/clip-1-muzica-pentru-reclame-film-" +
+					"calitate-24-biti-fisier-audio-wav-viteza-124-bpm-durata-105-min-90.wav");
+			AudioInputStream audioInputStream= AudioSystem.getAudioInputStream(archivoSonido);
+			clip= AudioSystem.getClip();
+			clip.open(audioInputStream);
+
+			clip.start();
+
+			Scene scene=new Scene();
+			scene.setOnKeyPressed(event->{
+				if(event.getCode()==KeyCode.X){
+					clip.stop();
+				}
+
+			});
+
+
+		} catch (UnsupportedAudioFileException e) {
+			throw new RuntimeException(e);
+		}catch (LineUnavailableException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+		 */
+
+	}
+
+
+	// Run graphics
+	private void run(GraphicsContext gc) {
+		gc.setFill(Color.grayRgb(20));
+		gc.fillRect(0, 0, WIDTH, HEIGHT);
+		gc.setTextAlign(TextAlignment.CENTER);
+		gc.setFont(Font.font(20));
+		gc.setFill(Color.WHITE);
+		gc.fillText("Score: " + player.score, 60,  20);
+
+		String explosion="door-bang-1wav-14449.wav";
+
+		if(gameOver) {
+			hiloMusical.explosionFinalizar(explosion);
+			gc.setFont(Font.font(35));
+			gc.setFill(Color.YELLOW);
+			gc.fillText("Game Over \n Your Score is: " + player.score + " \n Click to play again", WIDTH / 2, HEIGHT /2.5);
+			//	return;
+		}
+		univ.forEach(Universe::draw);
+		player.update();
+		player.draw();
+		player.posX = (int) mouseX;
+
+		Bombs.stream().peek(Rocket::update).peek(Rocket::draw).forEach(e -> {
+			if(player.colide(e) && !player.exploding) {
+				player.explode();
+				gameOver=true;
+
+				Rocket rocket=new Rocket();
+				rocket.muerteNave();
+			}
+		});
+
+		for (int i = shots.size() - 1; i >=0 ; i--) {
+			Shot shot = shots.get(i);
+			if(shot.posY < 0 || shot.toRemove)  {
+				shots.remove(i);
+				continue;
+			}
+			shot.update();
+			shot.draw();
+			for (Bomb bomb : Bombs) {
+				if(shot.colide(bomb) && !bomb.exploding) {
+					player.score++;
+					bomb.explode();
+					shot.toRemove = true;
+				}
+			}
+		}
+
+		for (int i = Bombs.size() - 1; i >= 0; i--){
+			if(Bombs.get(i).destroyed)  {
+				Bombs.set(i, newBomb());
+			}
+		}
+
+		gameOver = player.destroyed;
+		if(RAND.nextInt(10) > 2) {
+			univ.add(new Universe());
+		}
+		for (int i = 0; i < univ.size(); i++) {
+			if(univ.get(i).posY > HEIGHT)
+				univ.remove(i);
+		}
+	}
+
+	private Bomb newBomb() {
+		return new Bomb(50 + RAND.nextInt(WIDTH - 100), 0, PLAYER_SIZE, RAND.nextInt(BOMBS_IMG.length));
+	}
+
+	static int distance(int x1, int y1, int x2, int y2) {
+		return (int) Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+}
