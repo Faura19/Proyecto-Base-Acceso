1b635211fbe08ffb534d37042741216c08df52f4
Faura19 <161851845+Faura19@users.noreply.github.com>
2024-11-29 20:45:36 +0100

    Update HiloMusical.java

--- SpaceInvaders/src/main/java/com/spaceinvaders/spaceinvaders/HiloMusical.java
+++ SpaceInvaders/src/main/java/com/spaceinvaders/spaceinvaders/HiloMusical.java
@@ -1,23 +1,19 @@
 package com.spaceinvaders.spaceinvaders;
 
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.StackPane;
 
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class HiloMusical implements Runnable, Serializable {
 
-    private Clip clip;
+    private Clip clip1;
+    private Clip clip2;
     private String rutaCancion1;
     private String rutaCancion2;
     private String efectoEspecial;
+    private final Object monitor=new Object();
     private boolean musica=true;
 
     public HiloMusical(String rutaCancion2, String rutaCancion1) {
@@ -38,53 +34,76 @@ public class HiloMusical implements Runnable, Serializable {
 
     }
 
-    public void reproducirMusica(String rutaCancion1,String rutaCancion2) {
+    public void reproducirMusica(String rutaCancion1, String rutaCancion2) throws InterruptedException {
+        // Objeto sincronizador que usaremos para los hilos
+        Object semaforo = new Object();
+        musica = true;
 
-        Thread musicThread = new Thread(() -> {
+        // Hilo para la primera canción
+        Thread hiloCancion1 = new Thread(() -> {
             while (musica) {
-
                 try {
-                    // Reproduce la primera canción por 15 segundos
+                    // Reproducir la primera canción
                     File archivoSonido1 = new File(rutaCancion1);
                     AudioInputStream audioInputStream1 = AudioSystem.getAudioInputStream(archivoSonido1);
-                    clip = AudioSystem.getClip();
-                    clip.open(audioInputStream1);
-                    clip.start();
+                    clip1 = AudioSystem.getClip();
+                    clip1.open(audioInputStream1);
+                    clip1.start();
 
-                    // Espera 15 segundos antes de detener la canción
-                    Thread.sleep(15000);
-                    if (clip.isRunning()) {
-                        clip.stop();
-                        clip.close();
+                    synchronized (semaforo) {
+                        // Esperar a que termine la canción
+                        while (clip1.isRunning() && musica) {
+                            semaforo.wait();
+                        }
+                        clip1.stop();
+                        clip1.close();
+
+                        // Notificar al otro hilo que puede iniciar
+                        semaforo.notify();
                     }
-
                     if (!musica){
                         break;
                     }
-
-                    // Reproduce la segunda canción por 15 segundos
-                    File archivoSonido2 = new File(rutaCancion2);
-                    AudioInputStream audioInputStream2 = AudioSystem.getAudioInputStream(archivoSonido2);
-                    clip = AudioSystem.getClip();
-                    clip.open(audioInputStream2);
-                    clip.start();
-
-                    // Espera 15 segundos antes de detener la canción
-                    Thread.sleep(15000);
-                    if (clip.isRunning()) {
-                        clip.stop();
-                        clip.close();
-                    }
-
-                } catch (UnsupportedAudioFileException | IOException | LineUnavailableException |
-                         InterruptedException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
 
-        musicThread.start();
+        // Hilo para la segunda canción
+        Thread hiloCancion2 = new Thread(() -> {
+            while (musica) {
+                try {
+                    // Reproducir la segunda canción
+                    File archivoSonido2 = new File(rutaCancion2);
+                    AudioInputStream audioInputStream2 = AudioSystem.getAudioInputStream(archivoSonido2);
+                    clip2 = AudioSystem.getClip();
+                    clip2.open(audioInputStream2);
+                    clip2.start();
 
+                    synchronized (semaforo) {
+                        // Notificar al primer hilo que puede iniciar
+                        semaforo.notify();
+
+                        // Esperar a que termine la canción
+                        while (clip2.isRunning() && musica) {
+                            semaforo.wait();
+                        }
+                        clip2.stop();
+                        clip2.close();
+                    }
+                    if (!musica){
+                        break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Iniciar los hilos
+        hiloCancion1.start();
+        hiloCancion2.start();
     }
 
     public void explosionFinalizar(String efectoEspecial){
@@ -94,9 +113,9 @@ public class HiloMusical implements Runnable, Serializable {
             try {
                 File archivoSonido3 = new File(efectoEspecial);
                 AudioInputStream audioInputStream1 = AudioSystem.getAudioInputStream(archivoSonido3);
-                clip = AudioSystem.getClip();
-                clip.open(audioInputStream1);
-                clip.start();
+                clip1 = AudioSystem.getClip();
+                clip1.open(audioInputStream1);
+                clip1.start();
             } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
                 e.printStackTrace();
             }
@@ -108,9 +127,11 @@ public class HiloMusical implements Runnable, Serializable {
 
     public void detenerMusica(){
         musica=false;
-        if (clip.isRunning()){
-            clip.stop();
-            clip.close();
+        if ((clip1.isRunning() && clip1!=null) || (clip2.isRunning() && clip2!=null)){
+            clip1.stop();
+            clip1.close();
+            clip2.stop();
+            clip2.close();
         }
     }
 }
